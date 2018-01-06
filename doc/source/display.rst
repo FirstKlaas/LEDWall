48c0da2fca65a9c874b0cf98de2d4000e58a30c8
FirstKlaas <klaas.nebuhr@gmail.com>
2018-01-06 20:10:38 +0100

    Updated documentation

--- doc/source/display.rst
+++ doc/source/display.rst
@@ -5,7 +5,9 @@ Display
 
 Das Display bildet die Hauptklasse des Projektes. Ein Display Instanz repräsentiert ein physisches Panel. Mit der *update* Methode wird der Inhalt des Displays, also die Farbwerte für alle Pixel, über die serielle Schnittstelle an das Arduino Uno Board übertragen und somit der LED Streifen aktualisiert.
 
-.. autoclass:: ledwall.components.display.Display
+class Display
+-------------
+
+.. autoclass:: ledwall.components.Display
 	:members:
-	:undoc-members:
 
--- python/ledwall/components/display.py
+++ python/ledwall/components/display.py
@@ -176,10 +176,43 @@ class ColorTable(object):
 BYTES_PER_PIXEL = 3
 
 class Display(object):
-    """Main class for manipulation the LEDs
+    """Construktor. 
 
-    More explanation to come
-    """
+    Create a new instance of an led display. The baudrate has to match the baudrate of the arduino sketch.
+    Also the portName has to match the name of the port, the arduino is connected to. On windows os based
+    systems the name is something like 'COM3'.
+
+    The max value for the baudrate depends on the quality of the USB Cable as well as the length of the cable.
+    I tested successfully a baudrate of 1000000.
+
+    A very basic program could look like this:
+
+    .. code-block:: python
+    
+        from ledwall.components import *
+
+        d = Display(16,32)
+        
+        red   = Color(255,0,0)
+        green = Color(0,255,0)    
+
+        d.fill(green)
+        d.setPixel(0,3,red)
+        d.setPixel(14,23,red)
+
+        d.update()
+
+    :param cols: The number of columns of the display
+    :type cols: int
+
+    :param rows: The number of rows of the display.
+    :type rows: int
+
+    :param mode: The mode that the LEDs ar organized. Left-to-Right or Zig-Zag. Defaults to Display.MODE_LTR.
+    :type mode: int
+
+    :rtype: None
+    """    
 
     MODE_LTR     = 0
     MODE_ZIGZACK = 1
@@ -340,6 +373,34 @@ class Display(object):
         return (x ,y)
 
     def setPixel(self, x, y, color, update=False):
+        """Setting the color for a single pixel. 
+
+        Returns True, if setting the pixel was successful, Fales else. The color is specified by a color instance. The method
+        also accepts tuples or arrays as colors. The length has to be at least 3 and must contain the value for red, green, and
+        blue.
+
+        The following lines have the same effect.
+
+        .. code-block:: python
+
+            d.setPixel(0,3,red,True)
+            d.setPixel(0,3,(255,0,0),True)
+            d.setPixel(0,3,[255,0,0],True)
+            
+        :param x: The x position of the pixel. Must be a value with the following constraint: ``0 <= x < width``
+        :type x: int
+
+        :param y: The number of rows of the display.
+        :type y: int
+
+        :param color: The color for the pixel. If you want to deactivate or clear a pixel, just use black (0,0,0) as color value.
+        :type color: Color, tuple, list
+
+        :param update: If True, the display will be updated.
+        :type update: boolean
+
+        :rtype: boolean
+        """    
         if not self._testCoords(x,y):
             return False
 
@@ -348,6 +409,19 @@ class Display(object):
         return True
 
     def getPixel(self,x,y):
+        """Getting the color for a single pixel. 
+
+        Returns the color for the pixel located at (x,y). The x value must be within the the range: ``0 <= x < width''. 
+        The y value must be with the range: ``0 <= y < height``
+
+        :param x: The x position of the pixel. Must be a value with the following constraint: ``0 <= x < width``
+        :type x: int
+
+        :param y: The number of rows of the display.
+        :type y: int
+
+        :rtype: Color
+        """
         index = self._coordsToIndex(x,y) * 3
         return tuple(self._data[index:index+3])
 
@@ -360,12 +434,42 @@ class Display(object):
             index -= 1
 
     def hLine(self,row, color, update=False):
+        """Drawing a horizontal line in the specified color. 
+
+        The row value must be with the range: ``0 <= row < height``
+
+        :param row: The row to be filled.
+        :type row: int
+
+        :param color: The color for the row. If you want to deactivate or clear a row, just use black (0,0,0) as color value.
+        :type color: Color, tuple, list
+
+        :param update: If True, the display will be updated.
+        :type update: boolean
+
+        :rtype: None
+        """
         if (row < 0) or (row >= self.rows):
             raise ValueError('Rowindex out of bounds.', row)
         self[row*self.columns:((row+1)*self.columns)] = color    
         self.update(update)
 
     def vLine(self, column, color, update=False):
+        """Drawing a vertical line in the specified color. 
+
+        The column value must be with the range: ``0 <= column < width``
+
+        :param column: The column to be filled.
+        :type column: int
+
+        :param color: The color for the column. If you want to deactivate or clear a row, just use black (0,0,0) as color value.
+        :type color: Color, tuple, list
+        
+        :param update: If True, the display will be updated.
+        :type update: boolean
+
+        :rtype: None
+        """
         if (column < 0) or (column >= self.columns):
             raise ValueError('Columnindex out of bounds.', column)
 
@@ -378,6 +482,22 @@ class Display(object):
         return (row & 1) == 1
 
     def shiftRowRight(self, row, update=False):
+        """Shifts all pixels in the specified row to the right.
+
+        The method shift pixels to the right visually, because the method takes the mode into account. This means, if
+        the mode is ``Display.MODE_ZIGZAG`` and the row number is odd, then the pixela are shifted physically to the left.
+        But because this row reads left to right, the pixels are shifted visually to the right.
+
+        The pixel to the outermost right will be placed at the first column. So this method implements a rotation of the pixels.
+
+        :param row: The number of the row to be shifted. The row value must be with the range: ``0 <= row < height``
+        :type column: int
+
+        :param update: If True, the display will be updated.
+        :type update: boolean
+
+        :rtype: None
+        """
         if self.oddRow(row) and self._mode == Display.MODE_ZIGZACK:
             savedPixel = self.getPixel(self.columns-1,row)
             index = self._coordsToIndex(0,row, False) * 3
@@ -393,6 +513,19 @@ class Display(object):
         self.update(update)
 
     def shiftRight(self, update=False):
+        """Shifts all pixels to the right.
+
+        The method shift pixels to the right visually, because the method takes the mode into account. This means, if
+        the mode is ``Display.MODE_ZIGZAG`` and the row number is odd, then the pixela are shifted physically to the left.
+        But because this row reads left to right, the pixels are shifted visually to the right.
+
+        The pixel to the outermost right will be placed at the first column. So this method implements a rotation of the pixels.
+
+        :param update: If True, the display will be updated.
+        :type update: boolean
+
+        :rtype: None
+        """
         for row in range(self.rows):
             self.shiftRowRight(row)
 
@@ -414,6 +547,17 @@ class Display(object):
         self.update(update)
 
     def fill(self, color, update=False):
+        """Fills the LED display in the specified color.
+
+        :param color: The color for the display. If you want to deactivate or clear the panel, just use black (0,0,0) as color value.
+        :type color: Color, tuple, list
+        
+        :param update: If True, the display will be updated.
+        :type update: boolean
+
+        :rtype: None
+
+        """
         if isinstance(color, Color):
             self._data[::3]  = [color.red] * self.count
             self._data[1::3] = [color.green] * self.count
@@ -431,10 +575,44 @@ class Display(object):
         return NotImplemented
 
     def clear(self, update=False):
+        """Clears the display (sets all pixel to black)
+
+        :param update: If True, the display will be updated.
+        :type update: boolean
+
+        :rtype: None
+        """        
         self._data[:] = [0] * (BYTES_PER_PIXEL*self.count)
         self.update(update)
 
     def update(self, update=True):
+        """Updates the LED display
+
+        The internal framebuffer is send to the arduino board and the LED stripe gets updated.
+
+        The method takes framerate into account. If the time between this update and the last update
+        is less then the milliseconds per frame, the method will sleep 'til the end of the frame and 
+        update the data at the end of the frame.
+
+        Every time the LED display is updated, the framenumber will be increased by one. 
+
+        .. note::
+           If you are interested to see the time consumed by transferring the date to the arduino,
+           you can read the ransmissionInfo property after updating. The following code snippet shows
+           the basic usage:
+
+        Read out the timings for transmission:
+
+        .. code-block:: python
+
+            d.fill(Color(255,0,0))
+            print d.transmissionInfo
+
+        :param update: If True, the display will be updated.
+        :type update: boolean
+
+        :rtype: None
+        """
         if not update:
             return
 
