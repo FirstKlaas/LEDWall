c69589e924312bc2e92b58eb2c1b2769408f2ac5
FirstKlaas <klaas.nebuhr@gmail.com>
2018-01-16 08:33:35 +0100

    ESP Sketch for mqtt based client

---
+++ MqttClient/MqttClient.ino
@@ -0,0 +1,225 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <FastLED.h>
+
+#define DEBUG
+
+#define DATA_PIN     3
+
+#define CMD_INIT_PANEL      1
+#define CMD_CLEAR_PANEL     2
+#define CMD_FILL_PANEL      3
+#define CMD_PAINT_PANEL     4
+#define CMD_SET_PIXEL       5
+#define CMD_WRITE_RAW       6
+#define CMD_SHOW          255
+
+uint16_t numberOfLeds; 
+
+byte* leds;
+
+char ssid[]        = "***"; // ssid of your accesspoint     
+char password[]    = "***"; // password for your accesspoint 
+char mqtt_server[] = "***"; // IP or hostname of your mqtt server
+byte mac[6];                                  // Buffer for storing the MAC Address.
+uint16_t currentframeNr;
+
+WiFiClient* espClient;
+PubSubClient* client;
+
+void startWIFI(const char* ssid, const char* password) {
+  delay(1000);
+  #ifdef DEBUG
+  Serial.println(ssid);
+  #endif
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.persistent(false);
+    WiFi.mode(WIFI_OFF);   // this is a temporary line, to be removed after SDK update to 1.5.4
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      #ifdef DEBUG
+      Serial.print(".");
+      #endif
+    }
+  }
+
+  #ifdef DEBUG
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  #endif
+
+  WiFi.macAddress(mac);
+}
+
+uint16_t bytesToWord(uint8_t high,uint8_t low) {
+  uint16_t result = high; 
+  result <<= 8;
+  result |= low;
+  return result;
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  #ifdef DEBUG
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  Serial.print("Length ");
+  Serial.println(length);
+  #endif
+  
+  switch (payload[0]) {
+    case CMD_INIT_PANEL:
+      initPanel(payload);
+      break;
+
+    case CMD_SET_PIXEL:
+      setPixel(payload);
+      break;
+
+    case CMD_SHOW:
+      showPanel(payload);
+      break;
+
+    case CMD_WRITE_RAW:
+      cmdWriteRaw(payload);
+      break;  
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client->connected()) {
+    #ifdef DEBUG
+    Serial.print("Attempting MQTT connection...");
+    #endif
+    // Create a random client ID
+    String clientId = "LEDPANEL0001";
+    // Attempt to connect
+    if (client->connect(clientId.c_str())) {
+      #ifdef DEBUG
+      Serial.println("connected");
+      #endif
+      // Once connected, publish an announcement...
+      client->publish("DEVICELOGON", "LEDPANEL0001");
+      // ... and resubscribe
+      client->subscribe("LEDPANEL0001");
+    } else {
+      #ifdef DEBUG
+      Serial.print("failed, rc=");
+      Serial.print(client->state());
+      Serial.println(" try again in 5 seconds");
+      #endif
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+/**
+ * Write some bytes directly to the led buffer. 
+ * Currently no check for 'Index out of Bounds'
+ * violation.
+ * 
+ * Byte Position
+ * =============
+ * 0 : Command Byte. must be CMD_WRITE_RAW
+ * 1 : HIGH Byte of starting index
+ * 2 : LOW Byte of starting index
+ * 3 : HIGH Byte of number of bytes to write 
+ * 4 : LOW Byte of number of bytes to write
+ * 5 : First byte to write. All following bytes
+ *     will be copied directly to the ledbuffer.
+ *     
+ *     TODO: Check for violations 
+ *       - payload has fewer bytes than 
+ *         indicated by Byte 3 and 4
+ *       - Starting index is higher than 
+ *         leds buffer
+ *       - Starting index and the number 
+ *         of bytes to write is higher
+ *         than leds buffer
+ *     
+ *     @since: 15.01.2018
+ */
+void cmdWriteRaw(byte* cmdbuffer) {
+  if (cmdbuffer[0] != CMD_WRITE_RAW) return;
+  if (leds == NULL) return; 
+
+  uint16_t startIndex = bytesToWord(cmdbuffer[1], cmdbuffer[2]);
+  uint16_t numBytes = bytesToWord(cmdbuffer[3], cmdbuffer[4]);
+ 
+  memcpy(leds+startIndex,cmdbuffer+5,numBytes);
+}
+
+void initPanel(byte* cmdbuffer) {
+  if (cmdbuffer[0] != CMD_INIT_PANEL) return;
+  if (leds != NULL) free(leds); 
+  
+  //0 = WIDTH
+  //1 = HEIGHT
+  
+  const byte width  = cmdbuffer[1];
+  const byte height = cmdbuffer[2];
+  currentframeNr = bytesToWord(cmdbuffer[3],cmdbuffer[4]);
+  
+  numberOfLeds = width * height;
+  
+  leds = (byte*) malloc(numberOfLeds * 3);
+  
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>((CRGB*)leds, numberOfLeds);
+  memset8(leds,0,numberOfLeds * 3);
+  FastLED.showColor(CRGB::Green);
+}
+
+void showPanel(byte* cmdBuffer) {
+  if (cmdBuffer[0] != CMD_SHOW) return;
+  if (leds == NULL) return; 
+  
+  //memcpy(leds,cmdBuffer+1,numberOfLeds * 3);
+  FastLED.show(); 
+}
+
+void setPixel(byte* cmdBuffer) {
+  if (cmdBuffer[0] != CMD_SET_PIXEL) return;
+  if (leds == NULL) return; 
+  uint16_t index = (cmdBuffer[1] << 8) | cmdBuffer[2];
+  #ifdef DEBUG
+  Serial.print("Index: ");
+  Serial.println(index);
+  Serial.print("R: ");
+  Serial.println(cmdBuffer[3]);
+  Serial.print("G: ");
+  Serial.println(cmdBuffer[4]);
+  Serial.print("B: ");
+  Serial.println(cmdBuffer[5]);
+  #endif
+  
+  leds[index]   = cmdBuffer[3];
+  leds[index+1] = cmdBuffer[4];
+  leds[index+2] = cmdBuffer[5];
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  #ifdef DEBUG
+  Serial.println("Off we go");
+  #endif
+  delay(3000);
+  startWIFI(ssid, password);
+  espClient = new WiFiClient();
+  client = new PubSubClient(*espClient);
+  client -> setServer(mqtt_server, 1883);
+  client -> setCallback(callback);
+}
+
+void loop() {
+  if (!client->connected()) {
+    reconnect();
+  }
+  client->loop();
+}
