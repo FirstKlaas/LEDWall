0111f893263203693922f38d623e011861a35693
FirstKlaas <klaas.nebuhr@gmail.com>
2018-01-26 20:40:32 +0100

    First working version of the ProgMemSender

--- python/ledwall/components/progmemsender.py
+++ python/ledwall/components/progmemsender.py
@@ -2,17 +2,35 @@ from sender import Sender
 import os
 
 class ProgMemSender(Sender):
-	def __init__(self, path='.'):
-		Sender.__init__(self)
-		self._path = path
+    def __init__(self, path='.'):
+        Sender.__init__(self)
+        self._path = path
 
-	@property
-	def filename(self):
-		return "{}_{:d}".format(self.panel.id, self.panel.frame)
+    @property
+    def filename(self):
+        return "{}_{:d}.h".format(self.panel.id, self.panel.frame)
 
-	def update(self):
-		fd = os.open(os.join(self.path,filename), os.O_WRONLY | os.O_CREAT)
-		try:
-			os.write(fd, "Huhu")
-		finally:
-			os.close(fd)
+    @property
+    def path(self):
+        return self._path
+
+    @property 
+    def size(self):
+        return len(self.panel._data)
+
+    def update(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+            
+        fd = os.open(os.path.join(self.path,self.filename), os.O_WRONLY | os.O_CREAT)
+        try:
+            width  = self.panel.columns
+            height = self.panel.rows
+            os.write(fd, "const uint8_t {}[] PROGMEM = {} 0x{:02x}, 0x{:02x},\n".format('name', "{", width, height))
+            for i in range(0,self.size,8):
+                bytes = ['0x{:02x}, '.format(b) for b in self.panel._data[i:i+8]]
+                os.write(fd, "    {}\n".format(''.join(bytes)))
+            os.write(fd, "0x00};\n\n")
+
+        finally:
+            os.close(fd)
